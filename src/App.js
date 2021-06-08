26889617d86d8aeedbd3fba1f2e91037ceb8acf6
jay <jayrgeezy@gmail.com>
2021-06-08 18:44:12 +0300

    Shop Page, Collection preview and Item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from './pages/home/home';
 import {Route, Switch} from 'react-router-dom'
+import Shop from './pages/shop/shop';
+
+
 
 
 const HatsPage = ()=>{
   return(
-    <div>HatsPage</div>
+    <div> <h1>Hats Page</h1> </div>
   )
 }
 
@@ -15,7 +18,7 @@ function App() {
 
       <Switch>
         <Route exact path='/' component ={Home} />
-        <Route  path='/shop/hats' component ={HatsPage} />
+        <Route  path='/shop' component ={Shop} />
       </Switch>
     
     </div>
--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -56,6 +56,7 @@ class Directory extends Component{
                     <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
+                
             </div>
 
         )
